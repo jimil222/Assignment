9f3db14e4cd7da93e7806550c19817c0838ec0ab
Jimil Soni <jimilsoni926@gmail.com>
2025-07-05 12:01:59 +0530

    initial Vite + Tailwind CSS + TypeScript setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <h1 className="text-2xl font-semibold text-gray-800">
+        Hello, Vite + React + Tailwind!
+      </h1>
+    </div>
+  )
+}
+
+export default App
